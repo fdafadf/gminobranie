1e3b8e50cb0bf549d17d9afddd80d6ad32b3b647
fdafadf <fdafadf@gmail.com>
2021-12-01 17:21:54 +0100

    Wersja zerowa

--- js/View.js
+++ js/View.js
@@ -2,10 +2,11 @@ import { CanvasContextTransform } from "./CanvasContextTransform.js";
 
 export class View
 {
-    constructor({ borders_shape, borders_dbf, rides, width, height })
+    constructor({ borders_2_shape, borders_shape, borders_dbf, rides, width, height })
     {
         this.scale_x = width / (borders_shape.max_x - borders_shape.min_x);
         this.scale_y = height / (borders_shape.max_y - borders_shape.min_y);
+        this.borders_2_shape = borders_2_shape;
         this.borders_shape = borders_shape;
         this.borders_dbf = borders_dbf;
         this.rides = rides;
@@ -15,7 +16,6 @@ export class View
         this.canvas.addEventListener("mousemove", this._onMouseMove.bind(this));
         this.context = this.canvas.getContext("2d");
         this.context_transform = this._createCanvasContextTransform(this.context);
-        this._createCanvasPaths();
 
         // for (let ride of rides)
         // {
@@ -34,7 +34,7 @@ export class View
         //     }
         // }
 
-        this.buffer = this._createBuffer(this.borders_shape, width, height);
+        this.buffer = this._createBuffer();
         this._drawBuffer();
         document.body.appendChild(this.canvas);
     }
@@ -87,9 +87,9 @@ export class View
         return context_transform;
     }
 
-    _createBuffer(shape_file, width, height)
+    _createBuffer()
     {
-        let canvas = new OffscreenCanvas(width, height);
+        let canvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
         let context = canvas.getContext("2d");
         this._createCanvasContextTransform(context);
         context.imageSmoothingEnabled = false;
@@ -101,6 +101,14 @@ export class View
                 context.fillStyle = path.background_color;
                 context.stroke(path);
                 context.fill(path);
+            }
+        }
+        
+        for (let item of this.borders_2_shape.items)
+        {
+            for (let path of item.paths)
+            {
+                context.stroke(path);
             }
         }
 
@@ -130,38 +138,6 @@ export class View
         {
             this.selectedItem = null;
             this.draw();
-        }
-    }
-
-    _createCanvasPaths()
-    {
-        let background_color = 0;
-        let background_color_step = 360 / this.borders_shape.items.length;
-
-        for (let item of this.borders_shape.items)
-        {
-            item.paths = [];
-            let parts = [...item.parts].concat([item.points.length]);
-            
-            for (let k = 1; k < parts.length; k++)
-            {
-                let from = parts[k - 1];
-                let to = parts[k];
-                let path = new Path2D();
-                console.log(`item(${item.number}) from(${from}) to(${to})`);
-                path.moveTo(item.points[to * 2 - 2], item.points[to * 2 - 1]);
-                path.background_color = `hsl(${Math.round(background_color)}, 100%, 90%)`;
-                path.background_color_selected = `hsl(${Math.round(background_color)}, 100%, 50%)`;
-
-                for (let i = from; i < to; i++)
-                {
-                    path.lineTo(item.points[i * 2], item.points[i * 2 + 1]);
-                }
-
-                item.paths.push(path);
-            }
-
-            background_color += background_color_step;
         }
     }
 }
--- main.js
+++ main.js
@@ -21,12 +21,45 @@ async function loadRide(fileName)
     return path;
 }
 
-async function loadData({ borders_shape_url, borders_dbf_url, ride_urls })
+async function loadData({ borders_2_shape_url, borders_shape_url, borders_dbf_url, ride_urls })
 {
-    let borders_dbf = new DBaseFile(new DataView(await Http.request({ url: borders_dbf_url, responseType: 'arraybuffer' })));
+    let borders_2_shape = new ShapeFile(new DataView(await Http.request({ url: borders_2_shape_url, responseType: 'arraybuffer' })));
     let borders_shape = new ShapeFile(new DataView(await Http.request({ url: borders_shape_url, responseType: 'arraybuffer' })));
+    let borders_dbf = new DBaseFile(new DataView(await Http.request({ url: borders_dbf_url, responseType: 'arraybuffer' })));
     let rides = await Promise.all(ride_urls.map(loadRide));
-    return { borders_shape, borders_dbf, rides };
+    return { borders_2_shape, borders_shape, borders_dbf, rides };
+}
+
+function normalizeShape(shape)
+{
+    let background_color = 0;
+    let background_color_step = 360 / shape.items.length;
+
+    for (let item of shape.items)
+    {
+        item.paths = [];
+        let parts = [...item.parts].concat([item.points.length]);
+        
+        for (let k = 1; k < parts.length; k++)
+        {
+            let from = parts[k - 1];
+            let to = parts[k];
+            let path = new Path2D();
+            console.log(`item(${item.number}) from(${from}) to(${to})`);
+            path.moveTo(item.points[to * 2 - 2], item.points[to * 2 - 1]);
+            path.background_color = `hsl(${Math.round(background_color)}, 100%, 90%)`;
+            path.background_color_selected = `hsl(${Math.round(background_color)}, 100%, 50%)`;
+
+            for (let i = from; i < to; i++)
+            {
+                path.lineTo(item.points[i * 2], item.points[i * 2 + 1]);
+            }
+
+            item.paths.push(path);
+        }
+
+        background_color += background_color_step;
+    }
 }
 
 function normalizeRide(ride, translate_x, translate_y, scale_x, scale_y)
@@ -52,18 +85,21 @@ function normalizeRide(ride, translate_x, translate_y, scale_x, scale_y)
 
 // TODO: Konwersja koordynatów z standardu PUWG 1992 (EPSG:2180) do WGS84 (ESPG:4326)
 
-async function normalizeData({ borders_shape, borders_dbf, rides })
+async function normalizeData({ borders_2_shape, borders_shape, borders_dbf, rides })
 {
+    normalizeShape(borders_2_shape);
+    normalizeShape(borders_shape);
     let scale_x = (borders_shape.max_x - borders_shape.min_x) / (24.150000 - 14.116667);
     let scale_y = (borders_shape.max_y - borders_shape.min_y) / (54.833333 - 49.000000);
     rides = rides.map(ride => normalizeRide(ride, borders_shape.min_x, borders_shape.min_y, scale_x, scale_y));
-    return { borders_shape, borders_dbf, rides };
+    return { borders_2_shape, borders_shape, borders_dbf, rides };
 }
 
 async function onLoad()
 {
     let data_urls = 
     {
+        borders_2_shape_url: '/gminobranie/data/Województwa_Small.shp',
         borders_shape_url: '/gminobranie/data/Gminy_Small.shp',
         borders_dbf_url: '/gminobranie/data/Gminy.dbf',
         ride_urls: ['Ride_01.xml', 'Ride_02.xml', 'Ride_03.xml', 'Ride_04.xml', 'Ride_05.xml', 'Ride_06.xml']
